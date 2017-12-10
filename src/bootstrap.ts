21babb80686e2f632eeddfc4fd3b23bf8d396f8b
Andy <nnlyx@hotmail.com>
2017-12-09 22:37:25 -0700

    nodemon and start  server

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,8 +1,24 @@
-
+import { Server } from './server';
 
 export class Bootstrap {
 
     constructor() {
         console.log('Bootstrap app...');
+
+
+        let server = new Server();
+        server.Start();
     }
 }
+
+
+// import express = require('express');
+// let app = express();
+
+// app.get('/', function (request, response) {
+//     response.send('Hello World');
+// });
+
+// app.listen(3000);
+
+export default new Bootstrap();
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,39 @@
 
-import * as exporess from "express";
+import * as express from "express";
 
 
 export class Server {
 
-    public App: exporess.Application;
+    public App: express.Application;
 
+    port = process.env.port || 8000;
 
     constructor() {
-        this.App = exporess();
+        this.App = express();
     }
 
     public Start(): void {
+        this.Routes();
 
+        this.App.listen(this.port);
+
+        console.log(`Server started on port ${this.port}`);
+    }
+
+    private Routes(): void {
+
+        console.log(`Initialize routes`);
+
+        let router = express.Router();
+
+        router.get('/', (req, res, next) => {
+            res.json({
+                message: 'Hello World!'
+            });
+        });
+
+        this.App.use('/', router);
+
+        console.log(`Routes were configured`);
     }
 }
