500dd0054fe2beb58c2de90cc3b30d04430c48f4
AndrewLang <nnlyx@hotmail.com>
2017-12-14 16:38:53 -0700

    clean

--- src/server.ts
+++ src/server.ts
@@ -29,17 +29,6 @@ export class Server {
         this.App.listen(this.port);
 
         console.log(`Server started on port ${this.port}`);
-
-
-        // let db = new Common.DatabaseAccessor(`${__dirname}\\data\\MatrixBlog.db`);
-        // let rows = await db.Execute('select * from posts order by DatePublished DESC LIMIT 10');
-        // console.log(rows);
-
-        // this.TestActivator();
-
-        // this.TestDI();
-
-        this.TestReflect();
     }
 
     private Middleware(): void {
@@ -139,43 +128,6 @@ export class Server {
         console.log(type.length);
         // console.log(type.caller);
         console.log((<any>type).parameters);
-
-        // let proto = Object.getPrototypeOf(type);
-        // console.log(proto.constructor);
-        // let temp = proto();
-        // console.log(temp);
-
-        // let ctor = DI.Activator.GetParentCtor(type);
-        // console.log(ctor.prototype);
-
-
-        // let services = new DI.ServicecContainer();
-        // let token = { Token: 'LoggingService' };
-        // let errortoken = { Token: 'ExceptionLoggingService' };
-
-        // services.Register(DI.ServiceDescriptor.Singleton(token).UseType(LoggingService));
-        // services.Register(DI.ServiceDescriptor.Singleton(errortoken).UseType(ExceptionLoggingService));
-
-        // // services.Register(DI.ServiceDescriptor.Singleton(errortoken)
-        // //     .UseFactory(serviceProvider => {
-        // //         let loggingSvc = serviceProvider.GetService(token);
-
-        // //         return new ExceptionLoggingService(loggingSvc);
-        // //     }));
-
-        // console.log(services);
-
-        // let service = services.TryResolve<ILoggingService>(token);
-        // console.log(service);
-        // if (service) {
-        //     service.Debug('Got logging service');
-        // }
-
-        // let errorSvc = services.TryResolve<IExceptionHandlingService>(errortoken);
-        // console.log(errorSvc);
-        // if (errorSvc) {
-        //     errorSvc.Handle('fake exception');
-        // }
 
 
     }
